6cea2b53e629899aef020429b52cbee43a121cb6
mathislucka <mathis.lucka@gmail.com>
2025-04-23 15:51:04 +0100

    fix: add type annotations to test_check_last_commit_different_user method

--- tests/components/github/test_file_editor.py
+++ tests/components/github/test_file_editor.py
@@ -159,7 +159,7 @@ class TestGithubFileEditor:
         assert args2[0] == "https://api.github.com/user"
 
     @patch("requests.get")
-    def test_check_last_commit_different_user(self, mock_get, editor, mock_responses):
+    def test_check_last_commit_different_user(self, mock_get: Mock, editor: GithubFileEditor, mock_responses: dict) -> None:
         """Test checking if last commit was made by different user"""
         # First response for commits
         commits_response = Mock()
