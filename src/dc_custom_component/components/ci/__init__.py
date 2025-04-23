e56c2a923dc5548f11a06deef2216cabc0020cba
mathislucka <mathis.lucka@gmail.com>
2025-04-24 00:30:56 +0100

    feat: add CI components package init

---
+++ src/dc_custom_component/components/ci/__init__.py
@@ -0,0 +1,5 @@
+from .ci_output_parser import CIOutputParser
+from .branch_creator import CIBranchCreator
+from .documents_to_messages import CIDocumentToChatMessageConverter
+
+__all__ = ["CIOutputParser", "CIBranchCreator", "CIDocumentToChatMessageConverter"]
